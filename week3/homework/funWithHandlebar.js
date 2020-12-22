89e2674ad9c61dbca43878eb92f37f21f11e550f
AAlmosto <amisto73@gmail.com>
2020-12-22 15:46:27 +0100

    feedback changes

--- week3/homework/funWithHandlebar.js
+++ week3/homework/funWithHandlebar.js
@@ -1,34 +1,34 @@
-const hdbs = require('handlebars')
+const hdbs = require("handlebars");
 
-function getRandomWord(){
-  return Math.floor(Math.random()*7)
+function getRandomWord() {
+  return Math.floor(Math.random() * 7);
 }
-function drawCard(){
-  const subjects = [
-  'shark',
-  'popcorn',
-  'poison',
-  'fork',
-  'cherry',
-  'toothbrush',
-  'cannon',
-]
-const punchlines = [
-  'watch movie with',
-  'spread some love',
-  'put on cake',
-  'clean toilets',
-  'go to the moon',
-  'achieve world piece',
-  'help people learn programing',
-]
-const cardData ={
-  subjects: subjects[getRandomWord()],
-  punshlines:punchlines[getRandomWord()]
+const subjects = [
+  "shark",
+  "popcorn",
+  "poison",
+  "fork",
+  "cherry",
+  "toothbrush",
+  "cannon",
+];
+const punchline = [
+  "watch movie with",
+  "spread some love",
+  "put on cake",
+  "clean toilets",
+  "go to the moon",
+  "achieve world piece",
+  "help people learn programing",
+];
+function drawCard() {
+  const cardData = {
+    subjects: subjects[getRandomWord()],
+    punchline: punchline[getRandomWord()],
+  };
+  const card = "{{subjects}} is great to {{punchline}}";
+  const temp = hdbs.compile(card);
+  const result = temp(cardData);
+  return result;
 }
-const card = '{{subjects}} is great to {{punshlines}}'
-const temp = hdbs.compile(card)
-const result = temp(cardData)
-return result;
-};
-console.log(drawCard())
+console.log(drawCard());
--- week3/homework/norris-jokes.js
+++ week3/homework/norris-jokes.js
@@ -1,15 +1,13 @@
-const fetch = require('node-fetch');
-const url = 'https://api.icndb.com/jokes/random';
+const fetch = require("node-fetch");
+const url = "https://api.icndb.com/jokes/random";
 
-async function getNorrisJoke(){
-  try{
+async function getNorrisJoke() {
+  try {
     const response = await fetch(url);
-    const  data = await response.json();
-    console.log(data);
-    console.log(data.value.joke)
-  }catch(err){
-    console.log(err)
+    const data = await response.json();
+    console.log(data.value.joke);
+  } catch (err) {
+    console.log(err);
   }
-  
 }
 getNorrisJoke();
